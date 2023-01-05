e478198cda35d6db1cd48b3761c72f64cab60bc5
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-01-06 00:28:56 +0530

    Day2 has been completed

---
+++ Day3/p1.js
@@ -0,0 +1,7 @@
+// Problem 1: Print the numbers from the given starting point till ending point (including both start and end)
+let n = 10
+let i = 1
+while (i <= n) {
+  console.log(i)
+  i++
+}
---
+++ Day3/p3.js
@@ -0,0 +1,8 @@
+// Problem 3: Print the sum of all the multiples of 3 from 0 to the given limit
+let sum = 0
+n = 10
+for (let i = 1; i <= n; i++) {
+  sum = sum + i * 3
+}
+console.log(sum)
+
